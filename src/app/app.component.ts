b98b319ad616971a476119e221699aa5b056d18c
quanph14 <phquan.0493@gmail.com>
2022-11-28 10:51:31 +0700

    done 2811

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo-validator';
+  title = 'reactive-form';
+  countryList = [
+    {id: 1, name: 'Viet Nam'},
+    {id: 2, name: 'Canada'},
+    {id: 3, name: 'USA'},
+  ];
+  contactForm = new FormGroup({
+    firstname: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    lastname: new FormControl('', [Validators.maxLength(15)]),
+    email: new FormControl('', [Validators.required]),
+    gender: new FormControl('', [Validators.required]),
+    isMarried: new FormControl('', [Validators.required]),
+    country: new FormControl('', [Validators.required]),
+    address: new FormGroup({
+      city: new FormControl('', [Validators.required]),
+      street: new FormControl('',[Validators.required]),
+      pincode: new FormControl('', [Validators.required])
+    })
+  });
+  get firstname(){
+    return this.contactForm.get('firstname');
+  }
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
